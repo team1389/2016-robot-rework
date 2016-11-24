d380d739efc63b716a9657d2e5b25902ade042cd
ari mindell <amindell11@gmail.com>
2016-11-24 16:06:42 -0500

    cleanup

--- Hardware/src/com/team1389/hardware/interfaces/outputs/Velocity.java
+++
@@ -1,8 +0,0 @@
-package com.team1389.hardware.interfaces.outputs;
-
-public class Velocity{
-	public final double velocity;
-	public Velocity(double velocity) {
-		this.velocity = velocity;
-	}
-}
--- Hardware/src/com/team1389/hardware/util/RangeUtil.java
+++ Hardware/src/com/team1389/hardware/util/RangeUtil.java
@@ -1,11 +1,5 @@
 package com.team1389.hardware.util;
 
-import com.team1389.hardware.interfaces.outputs.OpenRangeOutput;
-
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Timer.Interface;
-import edu.wpi.first.wpilibj.Timer.StaticInterface;
-
 public class RangeUtil {
 	public static double map(double x,double in_min,double in_max,double out_min,double out_max){
 		  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -16,24 +10,5 @@ public class RangeUtil {
 	public static double applyDeadband(double val, double deadband){
         return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
 	}
-	public static void main(String[] args){
-		OpenRangeOutput printer=new OpenRangeOutput(){
-
-			@Override
-			public void set(double val) {
-				System.out.println(val);
-			}
-
-			@Override
-			public double min() {
-				return 0;
-			}
-
-			@Override
-			public double max() {
-				return 8192;
-			}
-			
-		};
-	}
+	
 }
