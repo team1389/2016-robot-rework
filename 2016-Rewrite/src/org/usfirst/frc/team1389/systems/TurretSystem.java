0ff71cc3ee9d2b0b6d80a0ab3a8f4f4178ec079a
ari mindell <amindell11@gmail.com>
2016-11-29 01:33:21 -0500

    saved changes to turretSystem

--- 2016-Rewrite/src/org/usfirst/frc/team1389/systems/TurretSystem.java
+++ 2016-Rewrite/src/org/usfirst/frc/team1389/systems/TurretSystem.java
@@ -1,31 +1,37 @@
 package org.usfirst.frc.team1389.systems;
 
+import com.team1389.hardware.inputs.software.LatchedDigitalInput;
 import com.team1389.hardware.inputs.software.PercentIn;
 import com.team1389.hardware.inputs.software.RangeIn;
 import com.team1389.hardware.outputs.software.PercentOut;
+import com.team1389.hardware.valueTypes.Angle;
 import com.team1389.hardware.valueTypes.Percent;
 import com.team1389.system.System;
 import com.team1389.watch.Info;
 
 public class TurretSystem extends System {
 	PercentOut voltRange;
-	public double joyVal;
 	RangeIn<Percent> joystick;
+	public double joyVal;
 
-	public TurretSystem(PercentOut voltageSystem, PercentIn joystickIn) {
+	RangeIn<Angle> turretAngle;
+	
+	LatchedDigitalInput zeroButton;
+	boolean toZero;
+	public TurretSystem(PercentOut voltageSystem, PercentIn joystickIn,RangeIn<Angle> turretAngle,LatchedDigitalInput zeroButton) {
 		this.voltRange = voltageSystem;
 		this.joystick = joystickIn.scale(.2);
+		this.turretAngle=turretAngle;
+		this.zeroButton=zeroButton;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Info[] getInfo() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -35,14 +41,14 @@ public class TurretSystem extends System {
 	}
 
 	@Override
-	public void defaultUpdate() {
-	voltRange.set(joyVal);	
+	public void updateInputMode() {
+		voltRange.set(joyVal);
 	}
 
 	@Override
 	public void getInput() {
 		joyVal = joystick.get();
-
+		
 	}
 
 }
