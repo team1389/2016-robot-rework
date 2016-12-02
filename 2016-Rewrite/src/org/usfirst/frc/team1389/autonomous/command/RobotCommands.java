b520bb122c20e11d21e048656084d36b2e332f7b
ari mindell <amindell11@gmail.com>
2016-12-02 13:05:10 -0500

    revised drivestraight command to use wpi pid rather than talon pid

---
+++ 2016-Rewrite/src/org/usfirst/frc/team1389/autonomous/command/RobotCommands.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team1389.autonomous.command;
+
+import org.usfirst.frc.team1389.robot.RobotConstants;
+import org.usfirst.frc.team1389.robot.RobotHardware;
+
+import com.team1389.auto.DriveCommands;
+import com.team1389.command_framework.command_base.Command;
+import com.team1389.configuration.PIDConfiguration;
+import com.team1389.configuration.PIDConstants;
+import com.team1389.control.SynchronousPIDController;
+import com.team1389.hardware.value_types.Percent;
+import com.team1389.hardware.value_types.Position;
+
+/**
+ * This class stores a set of drive commands for this particular robot's drivetrain
+ * 
+ * @author amind
+ * @see DriveCommands
+ */
+public class RobotCommands extends DriveCommands {
+
+	RobotHardware robot;
+
+	/**
+	 * inits the DriveCommands object
+	 */
+	public RobotCommands() {
+		super(RobotConstants.kWheelDiameter, RobotConstants.kMaxAcceleration, RobotConstants.kMaxDeceleration,
+				RobotConstants.kMaxVelocity);
+		robot = RobotHardware.getInstance();
+	}
+
+	/**
+	 * drives the given distance using drivetrain's position controllers
+	 * 
+	 * @param meters the distance to drive
+	 * @return the drive command
+	 */
+	public Command driveMetersTalonCommand(double meters) {
+		return driveMetersCommand(meters,
+				robot.leftDrive.getPositionOutput(new PIDConfiguration(new PIDConstants(0.03, 0, 0), true, false)),
+				robot.rightDrive.getPositionOutput(new PIDConfiguration(new PIDConstants(0.03, 0, 0), false, false)),
+				robot.leftDrive.getLeader().getPositionInput(), robot.rightDrive.getLeader().getPositionInput());
+	}
+
+	/**
+	 * drives the given distance using drivetrain's voltage controllers and position inputs
+	 * 
+	 * @param meters the distance to drive
+	 * @return the drive command
+	 */
+	public Command driveMetersCommand(double meters) {
+		SynchronousPIDController<Percent, Position> left = new SynchronousPIDController<Percent, Position>(
+				new PIDConfiguration(new PIDConstants(0.03, 0, 0), false, false),
+				robot.leftDrive.getLeader().getPositionInput(), robot.leftDrive.getVoltageOutput());
+		SynchronousPIDController<Percent, Position> right = new SynchronousPIDController<Percent, Position>(
+				new PIDConfiguration(new PIDConstants(0.03, 0, 0), false, false),
+				robot.rightDrive.getLeader().getPositionInput(), robot.rightDrive.getVoltageOutput());
+		return driveMetersCommand(meters, left, right);
+	}
+}
--- 2016-Rewrite/src/org/usfirst/frc/team1389/autonomous/modes/DriveStraight.java
+++ 2016-Rewrite/src/org/usfirst/frc/team1389/autonomous/modes/DriveStraight.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team1389.autonomous.modes;
 
-import org.usfirst.frc.team1389.autonomous.command.RobotDriveCommands;
+import org.usfirst.frc.team1389.autonomous.command.RobotCommands;
 
 import com.team1389.auto.AutoModeBase;
 import com.team1389.auto.AutoModeEndedException;
@@ -13,7 +13,7 @@ public class DriveStraight extends AutoModeBase{
 	private final double kDistanceToDrive = 5;
 	@Override
 	protected void routine() throws AutoModeEndedException {
-		runCommand(new RobotDriveCommands().driveMetersTalonCommand(kDistanceToDrive));
+		runCommand(new RobotCommands().driveMetersCommand(kDistanceToDrive));
 	}
 	
 }
