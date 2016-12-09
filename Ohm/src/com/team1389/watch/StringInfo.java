b6c7caf3e034fc8acd940f5e96cf1f30947b34b5
An0rak <phos4es@gmail.com>
2016-12-08 21:25:00 -0500

    Tried some javadoc shtuff, could be good, idk, couldnt use F3 so wasn't esactly sure on usage

--- Ohm/src/com/team1389/watch/StringInfo.java
+++ Ohm/src/com/team1389/watch/StringInfo.java
@@ -3,8 +3,12 @@ package com.team1389.watch;
 import edu.wpi.first.wpilibj.tables.ITable;
 
 public class StringInfo extends Info{
+	/**
+	 * Sets information (StringSource, Name) to class variables or super class
+	 * @param name the name of the hardware the source is from
+	 * @param source2 the input stream used to read from
+	 */
 	StringSource source;
-
 	public StringInfo(String name, StringSource source2) {
 		super(name);
 		this.source=source2;
