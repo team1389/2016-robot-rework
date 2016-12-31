aefb6c1f89b2ef186a99ac144976136b889ec631
Joshua Engels <joshuaengels@yahoo.com>
2016-12-31 10:59:26 -0500

    Commented CombinedProfile

--- Ohm/src/com/team1389/motion_profile/CombinedProfile.java
+++ Ohm/src/com/team1389/motion_profile/CombinedProfile.java
@@ -1,19 +1,38 @@
 package com.team1389.motion_profile;
 
+/**
+ * This is a single {@link MotionProfile} that represents the combination of two input {@link MotionProfile}s
+ * as if the end of the first one was combined to the beginning of the second. No check is made to ensure continuity of velocity.
+ *
+ */
 public class CombinedProfile extends MotionProfile {
 	MotionProfile profile;
 	MotionProfile profile2;
 
+	/**
+	 * 
+	 * @param profile The first part of the motion
+	 * @param profile2 The second part of the motion
+	 */
 	public CombinedProfile(MotionProfile profile, MotionProfile profile2) {
 		this.profile = profile;
 		this.profile2 = profile2;
 	}
 
+	/**
+	 * The duration is just the sum of the two components' durations
+	 */
 	@Override
 	public double getDuration() {
 		return profile.getDuration() + profile2.getDuration();
 	}
 
+	/**
+	 * Two cases: <br>
+	 * The time is less than the first's duration, in which case the position is just the first's position at that time. <br>
+	 * The time is more than the first's duration but less than the total duration, in which case the position is the
+	 * first's position at the first's duration plus the second's position at the inputed time minus the first's duration. 
+	 */
 	@Override
 	protected double providePosition(double time) {
 		if (time < profile.getDuration()) {
@@ -23,6 +42,9 @@ public class CombinedProfile extends MotionProfile {
 		}
 	}
 
+	/**
+	 * See documentation for {@link #providePosition(double) providePosition}, this is the same except for velocity instead of position.
+	 */
 	@Override
 	protected double provideVelocity(double time) {
 		if (time < profile.getDuration()) {
@@ -32,6 +54,10 @@ public class CombinedProfile extends MotionProfile {
 		}
 	}
 
+
+	/**
+	 * See documentation for {@link #providePosition(double) providePosition}, this is the same except for acceleration instead of position.
+	 */
 	@Override
 	protected double provideAcceleration(double time) {
 		if (time < profile.getDuration()) {
