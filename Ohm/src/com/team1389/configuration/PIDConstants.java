ab49bb89009cd5b363af112327ec657773db0680
ari mindell <amindell11@gmail.com>
2017-01-03 03:13:58 -0500

    only 1 package left

--- Ohm/src/com/team1389/configuration/PIDConstants.java
+++ Ohm/src/com/team1389/configuration/PIDConstants.java
@@ -6,10 +6,28 @@ package com.team1389.configuration;
  * @author Jacob Prinz
  */
 public final class PIDConstants {
+	/**
+	 * the default PID gains
+	 */
 	public static final PIDConstants DEFAULT = new PIDConstants(0, 0, 0);
-
-	public final double p, i, d;
-
+	/**
+	 * the proportional gain
+	 */
+	public final double p;
+	/**
+	 * the integral gain
+	 */
+	public final double i;
+	/**
+	 * the derivative gain
+	 */
+	public final double d;
+	
+	/**
+	 * @param p the proportional gain
+	 * @param i the integral gain
+	 * @param d the derivative gain
+	 */
 	public PIDConstants(double p, double i, double d) {
 		this.p = p;
 		this.i = i;
--- Ohm/src/com/team1389/configuration/PIDInput.java
+++ Ohm/src/com/team1389/configuration/PIDInput.java
@@ -7,21 +7,32 @@ import com.team1389.watch.CompositeWatchable;
 import com.team1389.watch.Watchable;
 import com.team1389.watch.input.NumberInput;
 
+/**
+ * represents a set of PID constants that can be tuned on the smart dashboard
+ * 
+ * @author amind
+ *
+ */
 public class PIDInput implements CompositeWatchable {
 	NumberInput p;
 	NumberInput i;
 	NumberInput d;
 	String name;
 
-	public PIDInput(String name, PIDConstants old, Consumer<PIDConstants> fun) {
+	/**
+	 * @param name the string identifier of this tuner
+	 * @param defaultVal the default PIDconstants
+	 * @param fun an action to perform when any one of the tuner values changes
+	 */
+	public PIDInput(String name, PIDConstants defaultVal, Consumer<PIDConstants> fun) {
 		this.name = name;
-		this.p = new NumberInput("p", old.p, (Double newP) -> {
+		this.p = new NumberInput("p", defaultVal.p, (Double newP) -> {
 			fun.accept(new PIDConstants(newP, i.get(), d.get()));
 		});
-		this.i = new NumberInput("i", old.i, (Double newI) -> {
+		this.i = new NumberInput("i", defaultVal.i, (Double newI) -> {
 			fun.accept(new PIDConstants(p.get(), newI, d.get()));
 		});
-		this.d = new NumberInput("d", old.d, (Double newD) -> {
+		this.d = new NumberInput("d", defaultVal.d, (Double newD) -> {
 			fun.accept(new PIDConstants(p.get(), i.get(), newD));
 		});
 	}
