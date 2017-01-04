1d8d8879bdac374d89fea5f216803cd532b333b3
ari mindell <amindell11@gmail.com>
2017-01-04 01:08:22 -0500

    fixed errors caused by small RangeIn refactor

--- 2016-Rewrite/src/org/usfirst/frc/team1389/robot/RobotSoftware.java
+++ 2016-Rewrite/src/org/usfirst/frc/team1389/robot/RobotSoftware.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1389.robot;
 import com.team1389.hardware.inputs.software.AngleIn;
 import com.team1389.hardware.inputs.software.DigitalIn;
 import com.team1389.hardware.value_types.Percent;
+import com.team1389.hardware.value_types.Position;
 import com.team1389.system.drive.DriveOut;
 
 public class RobotSoftware extends RobotHardware {
@@ -12,12 +13,14 @@ public class RobotSoftware extends RobotHardware {
 		return INSTANCE;
 	}
 
-	public AngleIn turretAngle = turretGyro.getAngleInput().sumInputs(navX.getYawInput()).setRange(-180, 180).getWrapped();
-	
+	public AngleIn turretAngle = turretGyro.getAngleInput().sumInputs(navX.getYawInput()).setRange(-180, 180)
+			.getWrapped();
+
 	public DigitalIn IRsensors = IRsensor1.getSwitchInput().combineOR(IRsensor2.getSwitchInput());
-	
+
 	public DriveOut<Percent> drive = new DriveOut<Percent>(leftDrive.getVoltageOutput(), rightDrive.getVoltageOutput());
-	
-	public AngleIn armVal = armPot.getAnalogInput().mapToRange(120, 0).setRange(0, 76).mapToRange(0, 90).setRange(0, 360).mapToAngle();
+
+	public AngleIn armVal = armPot.getAnalogInput().mapToRange(120, 0).setRange(0, 76).mapToRange(0, 90)
+			.setRange(0, 360).mapToAngle(Position.class);
 
 }
--- Dashboard-Test/src/input/Axis.java
+++ Dashboard-Test/src/input/Axis.java
@@ -24,6 +24,6 @@ public class Axis {
 				val -= kChangeRate * timer.get();
 			timer.zero();
 			return val;
-		}).limit();
+		}).clamp();
 	}
 }
