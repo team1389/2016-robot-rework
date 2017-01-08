62c9eced62e29cdaa7b432b0c90fd798603f6b68
Joshua Engels <joshuaengels@yahoo.com>
2017-01-08 10:57:18 -0500

    Started working on collision in the sim, i have some good ideas
    Basically we can check the intersection of each line of the rectangular
    bounding box of the robot with the wall if the robot and the wall
    actually intersect. Then we can get the point of intersection closest to
    each wheel and use torque to figure out interactions. I'm not entirely
    sure how it will play out, but hopefully it will work out in the end.

--- Dashboard-Test/src/simulation/SimulationRobot.java
+++ Dashboard-Test/src/simulation/SimulationRobot.java
@@ -1,9 +1,17 @@
 package simulation;
 
+import java.util.ArrayList;
+
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Line;
+import org.newdawn.slick.geom.Point;
+import org.newdawn.slick.geom.Polygon;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Transform;
 
 import com.team1389.hardware.inputs.software.AngleIn;
 import com.team1389.hardware.inputs.software.RangeIn;
@@ -38,15 +46,21 @@ public class SimulationRobot {
 	double rightDistance = 0;
 	double startX = 250;
 	double startY = 250;
+	ArrayList<Line> boundries;
 
-	public SimulationRobot() {
+	public SimulationRobot(ArrayList<Line> boundries) {
 		state.reset(Timer.getFPGATimestamp(), new RigidTransform2d(new Translation2d(), new Rotation2d()));
 		try {
 			robot = new Image("robot.png").getScaledCopy(68, 70);
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
+		this.boundries = boundries;
 
+	}
+	
+	public SimulationRobot(){
+		this(new ArrayList<Line>());
 	}
 
 	public void update() {
@@ -54,10 +68,14 @@ public class SimulationRobot {
 		right.update();
 		state.addFieldToVehicleObservation(Timer.getFPGATimestamp(),
 				state.getLatestFieldToVehicle().getValue()
-						.transformBy(RigidTransform2d.fromVelocity(new Kinematics(10, 23, .8)
-								.forwardKinematics(leftIn.get() - leftDistance, rightIn.get() - rightDistance))));
+				.transformBy(RigidTransform2d.fromVelocity(new Kinematics(10, 23, .8)
+						.forwardKinematics(leftIn.get() - leftDistance, rightIn.get() - rightDistance))));
 		leftDistance = leftIn.get();
 		rightDistance = rightIn.get();
+	}
+
+	public boolean checkCollision(Line l){
+		return getBoundingBox().intersects(l);
 	}
 
 	public DriveOut<Percent> getDrive() {
@@ -71,6 +89,7 @@ public class SimulationRobot {
 	}
 
 	public void render(GameContainer container, Graphics g) throws SlickException {
+
 		RigidTransform2d transform = state.getLatestFieldToVehicle().getValue();
 		Translation2d trans = transform.getTranslation();
 		Rotation2d rot = transform.getRotation();
@@ -79,6 +98,30 @@ public class SimulationRobot {
 		robot.setRotation((float) rot.getDegrees());
 		robot.setCenterOfRotation(34, 35);
 		robot.drawCentered(renderX, renderY);
+
+		g.setColor(Color.white);
+		g.rotate(renderX, renderY, (float) rot.getDegrees());
 		g.fillOval(renderX - 5, renderY - 5, 10, 10);
+		g.setColor(Color.pink);
+		g.setLineWidth(4);
+		g.drawRect(renderX - robot.getWidth() / 2, renderY - robot.getWidth() / 2, robot.getWidth(), robot.getHeight());
+		g.setColor(Color.blue);
+		g.draw(getBoundingBox());
+	}
+
+	private Polygon getBoundingBox(){
+		RigidTransform2d transform = state.getLatestFieldToVehicle().getValue();
+		Translation2d trans = transform.getTranslation();
+		Rotation2d rot = transform.getRotation();
+		float renderX = 2 * (float) (trans.getX() + startX);
+		float renderY = 2 * (float) (trans.getY() + startY);
+		Polygon r = new Polygon();
+		r.addPoint(renderX - robot.getWidth() / 2, renderY - robot.getWidth() / 2);
+		r.addPoint(renderX + robot.getWidth() / 2, renderY - robot.getWidth() / 2);
+		r.addPoint(renderX + robot.getWidth() / 2, renderY + robot.getWidth() / 2);
+		r.addPoint(renderX - robot.getWidth() / 2, renderY + robot.getWidth() / 2);
+
+		r.transform(Transform.createRotateTransform((float) rot.getRadians()));
+		return r;
 	}
 }
