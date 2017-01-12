de29eb26c0b5b491d70a0f149fe330911f809263
Quunii <kennethmiuraubw@gmail.com>
2017-01-12 17:22:54 -0500

    finished DoubleSolenoidHardware

--- Ohm/src/com/team1389/hardware/DoubleHardware.java
+++ Ohm/src/com/team1389/hardware/DoubleHardware.java
@@ -4,7 +4,10 @@ import java.util.Optional;
 
 import com.team1389.hardware.registry.Registry;
 import com.team1389.hardware.registry.port_types.PortInstance;
+import com.team1389.util.AddList;
 import com.team1389.watch.CompositeWatchable;
+import com.team1389.watch.Watchable;
+import com.team1389.watch.info.FlagInfo;
 
 /**
  * wraps any WPI hardware that uses two ports
@@ -78,5 +81,8 @@ public abstract class DoubleHardware<T extends PortInstance> implements Composit
 		String defaultName = getHardwareIdentifier() + " " + getPortA() + " " + getPortB();
 		return specificHardwareName.orElse(defaultName);
 	}
-
+	@Override
+	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem) {
+		return stem.put(new FlagInfo("port fault", () -> {return !port1.isPresent()==!port2.isPresent();}));
+}
 }
--- Ohm/src/com/team1389/hardware/outputs/hardware/DoubleSolenoidHardware.java
+++ Ohm/src/com/team1389/hardware/outputs/hardware/DoubleSolenoidHardware.java
@@ -1,43 +1,48 @@
 package com.team1389.hardware.outputs.hardware;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import com.team1389.hardware.DoubleHardware;
+import com.team1389.hardware.outputs.software.DigitalOut;
 import com.team1389.hardware.registry.Registry;
 import com.team1389.hardware.registry.port_types.PCM;
-import com.team1389.hardware.registry.port_types.PortInstance;
 import com.team1389.util.AddList;
 import com.team1389.watch.Watchable;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-public class DoubleSolenoidHardware extends DoubleHardware <PCM>{
-	Optional<DoubleSolenoid> doubleSolenoid1, doubleSolenoid2;
-	
+
+public class DoubleSolenoidHardware extends DoubleHardware<PCM> {
+	Optional<DoubleSolenoid> doubleSolenoid;
+
 	public DoubleSolenoidHardware(PCM requestedPort1, PCM requestedPort2, Registry registry) {
 		super(requestedPort1, requestedPort2, registry);
 	}
-
+	
+	//public  Consumer<DoubleSolenoid.Value> getSolenoidValOut(){
+		//return v->doubleSolenoid.ifPresent(w->w.set(v));
+	//}
+	public DigitalOut getDigitalOut(){
+		return new DigitalOut(val -> doubleSolenoid.ifPresent(w-> w.set((val==true)? DoubleSolenoid.Value.kForward: DoubleSolenoid.Value.kOff)));
+				}
 	@Override
 	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem) {
-		
-		return super.getSubWatchables(stem).put(getDigitalOut().getWatchable("state"));
+	return super.getSubWatchables(stem).put(getDigitalOut().getWatchable("state"));
 	}
 
 	@Override
 	public void failInit() {
-		// TODO Auto-generated method stub
-		
+		doubleSolenoid = Optional.empty();
 	}
 
 	@Override
 	protected String getHardwareIdentifier() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Double Solenoid";
 	}
 
 	@Override
 	public void init(PCM port1, PCM port2) {
-		// TODO Auto-generated method stub
-		
+		doubleSolenoid = Optional.of(new DoubleSolenoid(port1.index(), port2.index()));
 	}
 
 }
